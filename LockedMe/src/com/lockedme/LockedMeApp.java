1ec958dd843d83ef5a5c3eca4554692fa8104b9b
Bryan Gaskins <96695276+bgaskins@users.noreply.github.com>
2022-02-25 11:38:04 -0500

    Upload LockedMe.com Application

---
+++ LockedMe/src/com/lockedme/LockedMeApp.java
@@ -0,0 +1,15 @@
+package com.lockedme;
+
+
+public class LockedMeApp {
+
+		
+		
+	public static void main(String[] args) throws Exception {
+		
+		AppMenus.displayWelcomeScreen();
+		AppMenus.displayMenu();
+		AppMenus.fetchUserMenuOption();
+		
+	}
+}
